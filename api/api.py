16e7c24950ba572e34dc8484b9050008b131de80
Sylvia <sylviachach@gmail.com>
2020-11-22 14:13:30 +0300

    made changes to paint game

---
+++ api/api.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, jsonify
+import pusher
+import json
+
+
+app = Flask(__name__)
+
+pusher_client = pusher.Pusher(
+  app_id='1106312',
+  key='d1aacd6ddbe4dae09f34',
+  secret='cd9aece6afcff03ea515',
+  cluster='ap2',
+  ssl=True
+)
+
+@app.route('/')
+def index():
+   return render_template('index.html', name = 'Sylvia')
+
+@app.route('/paint',methods=['POST', 'GET'])
+def paint():
+   data = json.loads(request.data)data = json.loads(request.data)
+   pusher_client.trigger('painting', 'draw', data)
+   return jsonify(data)
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
